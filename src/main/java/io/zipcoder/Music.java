54bc00f1fb3bca888a2f30b92fbfe3a739debba6
Adam Bennett <nyoxidetwitter@gmail.com>
2020-02-13 22:30:36 -0500

    finished lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,44 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int left = 0;
+        int right = 0;
+
+        int i = startIndex;
+        int count = 0;
+        while (!this.playList[i].equals(selection)) {
+            if (i >= this.playList.length) {
+                i = 0;
+            } else if (i < 0) {
+                i = this.playList.length -1;
+            }
+            i++;
+            count++;
+            if (i >= this.playList.length) {
+                i = 0;
+            } else if (i < 0) {
+                i = this.playList.length -1;
+            }
+        }
+        right = count;
+
+        i = startIndex;
+        count = 0;
+        while (!this.playList[i].equals(selection)) {
+            if (i >= this.playList.length) {
+                i = 0;
+            } else if (i < 0) {
+                i = this.playList.length - 1;
+            }
+            i--;
+            count++;
+            if (i >= this.playList.length) {
+                i = 0;
+            } else if (i < 0) {
+                i = this.playList.length -1;
+            }
+        }
+        left = count;
+        return (left < right) ? left : right;
     }
 }
